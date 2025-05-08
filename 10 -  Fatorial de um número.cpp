ba52608ba5bbdc724ae3033ff7202192995148fb
Vicenzo <varandasvicenzo1@gmail.com>
2025-05-08 14:38:46 -0300

    Update 10 -  Fatorial de um número.cpp

--- 10 -  Fatorial de um número.cpp
+++ 10 -  Fatorial de um número.cpp
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <locale.h>
-#include <stdlib.h>
-#include <stdio.h>
 int main() {
 	int numero, F = 1, i ; // declarar variavel
 	printf ("digite um numero inteiro \n"); // solicitar resposta do usuario
 	scanf  ("%d", &numero); // ler resposta
-	for(int i = 2; i <= numero; i++){ // calculo para fatorar
-	    F = F * i;
+   for (int i = 1; i <= numero; i++){ // calculo para fatorar
+	    F *= i;  // calculo para fatorar
 	}
  printf("O fatorial de %d é %d\n", numero, F); // exibir resultado final
 
